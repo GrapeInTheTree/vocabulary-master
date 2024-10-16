2347034f4be1051882c2cef083decd2eec1ccbae
Danial Ahn <rightruth1202@gmail.com>
2024-10-16 12:45:01 +0900

    feat: add time field to the word model

--- internal/models/word.go
+++ internal/models/word.go
@@ -1,9 +1,13 @@
 package models
 
+import "time"
+
 type Word struct {
 	ID      string `json:"id"`
 	Word    string `json:"word"`
 	Meaning string `json:"meaning"`
 	Retries int    `json:"retries"`
 	// TODO: Add field for last time the word was edited including the retry count
+	CreatedAt      time.Time `json:"created_at"`
+	LastModifiedAt time.Time `json:"last_modified_at"`
 }
